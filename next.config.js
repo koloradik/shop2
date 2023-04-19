ca58c6a876f3d2b759b9f45296e81bb193d92ad3
T1myr <robobox2020@gmail.com>
2023-04-19 16:28:39 +0300

    nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatters: [
+      {
+        protocol: "https",
+        hostname: "**.clodinary.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
